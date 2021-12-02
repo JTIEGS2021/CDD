2dc1b0ebbfe88798ca292f51b9bb3624eca0c648
Jacob Tiegs <jtiegs@mcw.edu>
2021-12-01 18:37:24 -0600

    functioning with modules

--- R/app.R
+++ R/app.R
@@ -10,17 +10,13 @@ map(files, source)
 
 
 
-## define dataset
-## should load the saved dataset right away
-ui <- fluidPage(
 
+ui <- fluidPage(
   tabsetPanel(
   ## Welcome post
   welcomeUI("welcome"),
   datasetUI("dataset"),
   reportsUI("report"),
-  
-  ## show summary on select variable
   loadUI("load")
     # tabPanel("Load Data",
     #     ## input upload data
@@ -45,73 +41,74 @@ ui <- fluidPage(
   ))
 
 server <- function(input, output, session) {
-  ups <- tibble()
-  
-  ## takes upload data input and reads the dataset
-  up <- reactive({
-    req(input$upload)
-    ext <- tools::file_ext(input$upload$name)
-    xlsx <-  readxl::read_excel(input$upload$datapath)%>% select(1:11) 
-    ups <- xlsx
-    xlsx
-      }
-      )
-  
-  ## check variable names
-  up_name_check <- reactive({
-    ncheck <- c(
-    "pid"  ,                                                
-    "ui:2" ,                                                
-    "proc:2",                                              
-    "med:2",                                                
-    "ui_any:2" ,                                            
-    "age_bin_5:14" ,                                        
-    "charlson_comorb:5 (1-5, 1 being mild 5 being extreme)",
-    "zip3:20 (not accurately distributed)",                 
-    "date_dx_proc_med:91" ,                                 
-    "referal:3" ,                                           
-    "PRO_1:4"   
-    )
-    names <- up() %>% names() 
-    checked <- (names == ncheck) 
-    df <- tibble(names,checked)
-    df
-  })
-  
-
-  # print up_name_check
-  output$up_name_check <- renderPrint({
-    up_name_check()
-  })
-  
-  ## print confirmation text
-  output$up_conf <- renderText({
-    df <- up_name_check()
-    if(all(df$checked)){
-      "Variables Match"
-      # call function save
-    } else 
-      "Variables Do Not Match"
-      # dont call function save
-  })
-
-  ## output head from upload dataset 
-  output$head <- renderTable({
-    head(up(), input$upload_n)
-  })
-  
-  # render the save button if all up_name_che()$checked are true
-  output$up_save <-
-    renderUI(expr = if (all(up_name_check()$checked)) {
-      submitButton()
-    } else {
-      NULL
-    })
-  
-  observeEvent(input$up_save,  reactive({
-    df <- rbind(df,up)
-    saveRDS(ups, file="df.rds")
-   }))
+  loadServer("load")
+  # ups <- tibble()
+  # 
+  # ## takes upload data input and reads the dataset
+  # up <- reactive({
+  #   req(input$upload)
+  #   ext <- tools::file_ext(input$upload$name)
+  #   xlsx <-  readxl::read_excel(input$upload$datapath)%>% select(1:11) 
+  #   ups <- xlsx
+  #   xlsx
+  #     }
+  #     )
+  # 
+  # ## check variable names
+  # up_name_check <- reactive({
+  #   ncheck <- c(
+  #   "pid"  ,                                                
+  #   "ui:2" ,                                                
+  #   "proc:2",                                              
+  #   "med:2",                                                
+  #   "ui_any:2" ,                                            
+  #   "age_bin_5:14" ,                                        
+  #   "charlson_comorb:5 (1-5, 1 being mild 5 being extreme)",
+  #   "zip3:20 (not accurately distributed)",                 
+  #   "date_dx_proc_med:91" ,                                 
+  #   "referal:3" ,                                           
+  #   "PRO_1:4"   
+  #   )
+  #   names <- up() %>% names() 
+  #   checked <- (names == ncheck) 
+  #   df <- tibble(names,checked)
+  #   df
+  # })
+  # 
+  # 
+  # # print up_name_check
+  # output$up_name_check <- renderPrint({
+  #   up_name_check()
+  # })
+  # 
+  # ## print confirmation text
+  # output$up_conf <- renderText({
+  #   df <- up_name_check()
+  #   if(all(df$checked)){
+  #     "Variables Match"
+  #     # call function save
+  #   } else 
+  #     "Variables Do Not Match"
+  #     # dont call function save
+  # })
+  # 
+  # ## output head from upload dataset 
+  # output$head <- renderTable({
+  #   head(up(), input$upload_n)
+  # })
+  # 
+  # # render the save button if all up_name_che()$checked are true
+  # output$up_save <-
+  #   renderUI(expr = if (all(up_name_check()$checked)) {
+  #     submitButton()
+  #   } else {
+  #     NULL
+  #   })
+  # 
+  # observeEvent(input$up_save,  reactive({
+  #   df <- rbind(df,up)
+  #   saveRDS(ups, file="df.rds")
+  #  }))
   
   
   
--- R/load.R
+++ R/load.R
@@ -8,26 +8,95 @@ user_base <- tibble(
 
 
 loadUI <- function(id) {
-  ns <- NS(id)
   
-  tabPanel("Load Data",
-         ## input upload data
-         fileInput("upload", NULL, accept = c(".xlsx")),
-         
+  tabPanel("load",
+         # input upload data
+         fileInput(NS(id,"upload"), NULL, accept = c(".xlsx")),
          ## input n rows
-         numericInput("upload_n", "Check Rows", value = 5, min = 1, step = 1),
-         
+         numericInput(NS(id,"upload_n"), "Check Rows", value = 5, min = 1, step = 1),
          ## output head
-         tableOutput("head"),
-         
+         tableOutput(NS(id,"head")),
          ## name check
-         verbatimTextOutput("up_name_check"),
-         
+         verbatimTextOutput(NS(id,"up_name_check")),
          ## confirmation box
-         textOutput("up_conf"),
-         
+         textOutput(NS(id,"up_conf")),
          ## save to full dataset
-         uiOutput('up_save')
+         uiOutput(NS(id,"up_save"))
+  )
+}
+
+loadServer <- function(id) {
+  moduleServer(
+    id,
+    function(input, output, session) {
+      ups <- tibble()
+      
+      ## takes upload data input and reads the dataset
+      up <- reactive({
+        req(input$upload)
+        ext <- tools::file_ext(input$upload$name)
+        xlsx <-  readxl::read_excel(input$upload$datapath)%>% select(1:11) 
+        ups <- xlsx
+        xlsx
+      }
+      )
+      
+      ## check variable names
+      up_name_check <- reactive({
+        ncheck <- c(
+          "pid"  ,                                                
+          "ui:2" ,                                                
+          "proc:2",                                              
+          "med:2",                                                
+          "ui_any:2" ,                                            
+          "age_bin_5:14" ,                                        
+          "charlson_comorb:5 (1-5, 1 being mild 5 being extreme)",
+          "zip3:20 (not accurately distributed)",                 
+          "date_dx_proc_med:91" ,                                 
+          "referal:3" ,                                           
+          "PRO_1:4"   
+        )
+        names <- up() %>% names() 
+        checked <- (names == ncheck) 
+        df <- tibble(names,checked)
+        df
+      })
+      
+      
+      # print up_name_check
+      output$up_name_check <- renderPrint({
+        up_name_check()
+      })
+      
+      ## print confirmation text
+      output$up_conf <- renderText({
+        df <- up_name_check()
+        if(all(df$checked)){
+          "Variables Match"
+          # call function save
+        } else 
+          "Variables Do Not Match"
+        # dont call function save
+      })
+      
+      ## output head from upload dataset 
+      output$head <- renderTable({
+        head(up(), input$upload_n)
+      })
+      
+      # render the save button if all up_name_che()$checked are true
+      output$up_save <-
+        renderUI(expr = if (all(up_name_check()$checked)) {
+          submitButton()
+        } else {
+          NULL
+        })
+      
+      observeEvent(input$up_save,  reactive({
+        df <- rbind(df,up)
+        saveRDS(ups, file="df.rds")
+      }))
+    }
   )
 }
 
@@ -35,24 +104,3 @@ loadUI <- function(id) {
 
 
 
-
-
-
-# 
-# linkedScatter <- function(input, output, session, data, left, right) {
-#   # Yields the data frame with an additional column "selected_"
-#   # that indicates whether that observation is brushed
-#   dataWithSelection <- reactive({
-#     brushedPoints(data(), input$brush, allRows = TRUE)
-#   })
-#   
-#   output$plot1 <- renderPlot({
-#     scatterPlot(dataWithSelection(), left())
-#   })
-#   
-#   output$plot2 <- renderPlot({
-#     scatterPlot(dataWithSelection(), right())
-#   })
-#   
-#   return(dataWithSelection)
-# }
